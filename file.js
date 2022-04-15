c2912fc4bb25d8f1727674cc0d84f2fec6c5497c
ganeshbidgar <ganeshbidgar87@gmail.com>
2022-04-15 12:45:06 +0530

    first commit

--- file.js
+++ file.js
@@ -1,1 +1,15 @@
 console.log('Hallo')
+
+//git init
+
+//staging -------git add
+
+//local repo-----git commit -m "first log"
+
+//configure email and username
+
+//git commit -m "first log"
+
+// open github 
+
+//repo
